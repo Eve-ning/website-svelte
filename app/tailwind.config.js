567354361cc9dfacfadd54c306165d347378f90c
Evening <johnchangqi@hotmail.com>
2023-12-13 10:03:12 +0800

    Update tailwind.config.js

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./src/**/*.{html,js,svelte,ts}'],
-    // darkMode: 'class',
+    content: ['./src/**/*.{html,js,svelte,ts,css}'], // darkMode: 'class',
     theme: {
         container: {
             center: true
