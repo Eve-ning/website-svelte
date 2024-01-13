71d9314697d5f630ba848cf89bc092960745ed24
Evening <johnchangqi@hotmail.com>
2024-01-13 23:56:35 +0800

    Revert "Update svelte.config.js"
    This reverts commit d93e5416c1e468e19674282a6c4c832fcd6a7666.

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -19,7 +19,7 @@ const config = {
       // strict: true
     }),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/sanity-web/tree/gh-pages' : ''
+      base: process.env.NODE_ENV === 'production' ? '/sanity-web' : ''
     }
   }
 }
