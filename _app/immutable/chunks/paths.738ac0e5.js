aaf5c14d1141ceb102c9715c34eddef915781ba1
Eve-ning <Eve-ning@users.noreply.github.com>
2024-01-13 15:53:24 +0000

    Deploying to gh-pages from @ Eve-ning/sanity-web@46a32ce5391ea22176d16b7de8c17ca2d7a63b0f 🚀

---
+++ _app/immutable/chunks/paths.738ac0e5.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_iyi3ll)==null?void 0:s.base)??"/sanity-web/tree/gh-pages";var e;const t=((e=globalThis.__sveltekit_iyi3ll)==null?void 0:e.assets)??a;export{t as a,a as b};
