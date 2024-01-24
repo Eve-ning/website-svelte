96b613b7fa02eaceb2fff6080f640c298c5bbe5a
Evening <johnchangqi@hotmail.com>
2024-01-24 23:35:36 +0800

    Add mermaid support

--- app/src/routes/post/[slug]/+page.svelte
+++ app/src/routes/post/[slug]/+page.svelte
@@ -4,19 +4,34 @@
   import {marked} from 'marked'
   import markedKatex from 'marked-katex-extension'
   import Hero from '../../../components/Hero.svelte'
+  import mermaid from 'mermaid';
 
+  mermaid.initialize({
+    theme: 'dark',
+    // themeVariables: {
+    //   'background': "#000",
+    //   'primaryColor': '#BB2528',
+    //   'primaryTextColor': '#fff',
+    //   'primaryBorderColor': '#7C0000',
+    //   'lineColor': '#F8B229',
+    //   'secondaryColor': '#006100',
+    //   'tertiaryColor': '#fff'
+    // }
+  })
   export let data: PageData
   marked.use(markedKatex({displayMode: true, output: 'mathml'}))
-  const renderer = new marked.Renderer()
-
-  renderer.code = function(text) {
-    return `<div class="mockup-code"><pre><code>${text}</code></pre></div>`
-  }
+  const renderer = new marked.Renderer();
+  renderer.code = function (code, language) {
+    if (code.match(/^sequenceDiagram/) || code.match(/^graph/)) {
+      return '<pre class="mermaid bg-transparent grid justify-items-center">' + code + '</pre>';
+    } else {
+      return '<pre class="bg-transparent"><code>' + code + '</code></pre>';
+    }
+  };
 
 </script>
 
-<Hero half="{true}" title="{data.title ?? '-'}">
-
+<Hero title="{data.title ?? '-'}">
   <div slot="post">
     <h3>{data.excerpt}</h3>
     <p>{formatDate(data._createdAt)}</p>
