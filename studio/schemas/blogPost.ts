a88ff24d420cdaee96e2ffea0891f7d6cca03f17
Eve-ning <johnchangqi@hotmail.com>
2024-01-27 21:39:50 +0800

    Update studio

--- studio/schemas/blogPost.ts
+++ studio/schemas/blogPost.ts
@@ -1,4 +1,5 @@
 import {defineField, defineType} from 'sanity'
+import {CustomMarkdownInput} from '../plugins/MarkdownInputCustomPreview'
 
 export default defineType({
   name: 'blogPost',
@@ -30,6 +31,7 @@ export default defineType({
       name: 'body',
       title: 'Body',
       type: 'markdown',
+      components: {input: CustomMarkdownInput},
     }),
   ],
   preview: {
