bdbaae144ac9a096ea8fc38be1345115064c0567
Eve-ning <johnchangqi@hotmail.com>
2024-01-28 08:37:04 +0800

    Adjust Hero Sizing

--- app/src/components/Hero.svelte
+++ app/src/components/Hero.svelte
@@ -4,10 +4,10 @@
 <div
         class="hero py-10  bg-[conic-gradient(at_bottom,_var(--tw-gradient-stops))] from-slate-900 via-purple-950 to-slate-900">
     <div class="hero-content text-center">
-        <div class="max-w-md">
+        <div class="max-w-screen-lg">
             <div class="shadow-sm shadow-purple-400 rounded-2xl p-10">
-                <h1 class="text-5xl font-bold">{title}</h1>
-                <div class="py-6">
+                <h1 class="text-4xl font-bold">{title}</h1>
+                <div class="py-2">
                     <slot name="pre"/>
                 </div>
 
