68758e5e51cf4fbdb9dfee7ebcdb97dfeb8dd4c2
Evening <johnchangqi@hotmail.com>
2024-01-28 22:39:43 +0800

    Remove unused blockContent

--- studio/schemas/index.ts
+++ studio/schemas/index.ts
@@ -1,6 +1,5 @@
-import blockContent from './blockContent'
 import blogPost from './blogPost'
 import galleryImage from './galleryImage'
 import expPost from './expPost'
 
-export const schemaTypes = [blockContent, blogPost, galleryImage, expPost]
+export const schemaTypes = [blogPost, galleryImage, expPost]
