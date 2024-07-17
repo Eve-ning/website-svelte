92f2894d53282c38a74fb5312c68ecc65d74782a
Eve-ning <johnchangqi@hotmail.com>
2024-07-17 18:13:16 +0800

    Update pathing for website

--- _app/immutable/chunks/paths.9df24610.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_d2b3te)==null?void 0:s.base)??"/website-svelte";var e;const a=((e=globalThis.__sveltekit_d2b3te)==null?void 0:e.assets)??t;export{a,t as b};
---
+++ _app/immutable/chunks/paths.b619a574.js
@@ -0,0 +1,1 @@
+var s;const l=((s=globalThis.__sveltekit_ljz3lg)==null?void 0:s.base)??"/website-svelte";var e;const t=((e=globalThis.__sveltekit_ljz3lg)==null?void 0:e.assets)??l;export{t as a,l as b};
